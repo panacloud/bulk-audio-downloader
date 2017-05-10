86e211866bce21d654e165ef120a54c28560ef5b
zeeshanhanif <zeeshanhanif@gmail.com>
2017-05-10 09:05:12 +0500

    Add search all in client

--- client/src/store/middleware/epicMiddleware.js
+++ client/src/store/middleware/epicMiddleware.js
@@ -7,6 +7,12 @@ export default class AudioListEpic{
         return actions$.ofType(AudioActions.GET_DATA)
         .mergeMap(() => {
             return Observable.ajax("https://api.github.com/users/farooqmajeed/repos")
+            //return Observable.ajax("/search")
+            //return Observable.ajax({"/read"})
+            //return Observable.ajax({ url: '/read', method: 'GET', responseType: 'audio/mp4' })
+            .do((data)=>{
+                console.log("data >> ",data);
+            })
             .pluck("response")
             .switchMap((data) =>{
                 return Observable.of(AudioActions.getAudioListSuccessfull(data))
