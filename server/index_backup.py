59cff121584ecbe36dcb591c165cdf610e9539f9
zeeshanhanif <zeeshanhanif@gmail.com>
2017-05-17 21:16:20 +0500

    search only from phone number which is middel part of the file name '013-0528006762-1494307249.376673'

--- server/index_backup.py
+++ server/index_backup.py
@@ -4,6 +4,13 @@ import os
 import io
 import zipfile
 
+#app = Flask(__name__, static_url_path='/test');
+
+#app = Flask(__name__,
+#            static_url_path='',
+#            template_folder='../test',
+#            static_folder='../test');
+
 app = Flask(__name__,
             static_url_path='',
             template_folder='../client/build',
@@ -33,6 +40,17 @@ def search():
     return json.dumps(fileList)
     #return json.dumps(["Hello","World","Data","Test"])
 
+@app.route("/search2", methods=['GET'])
+#@app.route("/search")
+    
+def search2():  
+    
+    # filename = request.form['filename']   
+    fileToSearch = ""
+    fileList = searchDirectory(fileToSearch)
+    return json.dumps(fileList)
+#return json.dumps(["Hello","World","Data","Test"])
+
 
 def searchDirectory(fileToSearch):
     fileList = traverseDirectory(baseSearchPath,fileToSearch)
@@ -45,8 +63,7 @@ def traverseDirectory(baseSearchPath, fileToSearch):
     print(data)
 
     #iterating over file time and then individual files then add required file in list
-    finalList = [item[0] + os.path.sep + file for item in data for file in item[2] if verifyFileName(file,fileToSearch)]
-    #finalList = [item[0] + os.path.sep + file for item in data for file in item[2] if fileToSearch in file]
+    finalList = [item[0] + os.path.sep + file for item in data for file in item[2] if fileToSearch in file]
     #finalList = [item[0]+os.path.sep+file for item in data for file in item[2] if file.startswith(fileToSearch)]
 
     print("-----")  
@@ -54,15 +71,32 @@ def traverseDirectory(baseSearchPath, fileToSearch):
     print("-----")
     return finalList
 
+def traverseDirectory2(baseSearchPath, fileToSearch):
+    data = os.walk(baseSearchPath)
+    for i in data:
+        print(i)
+        print(i[2])
+        verifyFileName(i[2],fileToSearch)
 
-def verifyFileName(filePath, fileToSearch):
-    filePathArray = filePath.split(os.path.sep)
-    fileName = filePathArray[(len(filePathArray)) - 1]
-    fileNameRequired = fileName.split("-")[1]
-    return fileToSearch in fileNameRequired
+def verifyFileName(filesNameList, fileToSearch):
+    for name in filesNameList:
+        print(name)
+        print(name.find(fileToSearch))
+        print(fileToSearch in name)
 
 
 
+@app.route("/read")
+def reading():
+    path_to_file = ["D:\\developmentData\\DeepLearning\\song1.mp4","D:\\developmentData\\DeepLearning\\song2.mp4"]
+    for file in path_to_file:
+        fileName = file.split("\\")
+        return send_file(
+            file,
+            mimetype="audio/mp4",
+            as_attachment=True,
+            attachment_filename=fileName[len(fileName)-1])
+    
 
 
 @app.route("/downloadFile", methods=['GET'])
@@ -82,6 +116,18 @@ def readZipFile():
             #zf.writestr(data, individualFile['fileData'])
             with open(individualFile,"rb") as file_object:
                 zf.writestr(data,file_object.read())
+    memory_file.seek(0)
+    return send_file(memory_file, attachment_filename='capsule.zip', as_attachment=True)
+
+
+@app.route("/downloadFileOld")
+def readZipFile2():
+    memory_file = io.BytesIO()
+
+    with zipfile.ZipFile(memory_file, 'w') as zf:
+        files = ["D:\\developmentData\\DeepLearning\\song1.mp4","D:\\developmentData\\DeepLearning\\song2.mp4"]
+        for individualFile in files:
+            zf.write(individualFile.split("\\")[3])
     memory_file.seek(0)
     return send_file(memory_file, attachment_filename='capsule.zip', as_attachment=True)
 
